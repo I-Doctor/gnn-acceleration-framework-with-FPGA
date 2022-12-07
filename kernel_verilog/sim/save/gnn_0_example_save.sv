0f5abcdfaab78ff0cccad4b324312dc9cb99e781
Chiyue Wei <1780277455@qq.com>
2022-12-07 21:03:27 +0800

    add save module testbench

---
+++ kernel_verilog/sim/save/gnn_0_example_save.sv
@@ -0,0 +1,185 @@
+// This is a generated file. Use and modify at your own risk.
+////////////////////////////////////////////////////////////////////////////////
+// default_nettype of none prevents implicit wire declaration.
+`default_nettype none
+
+module gnn_0_example_save #(
+  parameter integer SAVE_INST_LENGTH         = 96,
+  parameter integer C_M_AXI_ADDR_WIDTH       = 64 ,
+  parameter integer C_M_AXI_DATA_WIDTH       = 512,
+  parameter integer C_XFER_SIZE_WIDTH        = 32,
+  parameter integer C_ADDER_BIT_WIDTH        = 32
+)
+(
+  // System Signals (don't change)
+  input wire                                    aclk               ,
+  input wire                                    areset             ,
+  // Extra clocks (don't change)
+  input wire                                    kernel_clk         ,
+  input wire                                    kernel_rst         ,
+  // AXI4 master interface (don't change)
+  output wire                                   m_axi_awvalid      ,
+  input wire                                    m_axi_awready      ,
+  output wire [C_M_AXI_ADDR_WIDTH  -1:0]        m_axi_awaddr       ,
+  output wire [8-1:0]                           m_axi_awlen        ,
+  output wire                                   m_axi_wvalid       ,
+  input wire                                    m_axi_wready       ,
+  output wire [C_M_AXI_DATA_WIDTH  -1:0]        m_axi_wdata        ,
+  output wire [C_M_AXI_DATA_WIDTH/8-1:0]        m_axi_wstrb        ,
+  output wire                                   m_axi_wlast        ,
+  input wire                                    m_axi_bvalid       ,
+  output wire                                   m_axi_bready       ,
+  // read from buffer port commected with buffer, use it
+  output logic                                  save_read_buffer_addr_valid,
+  output logic [11-1:0]                         save_read_buffer_addr      ,
+  input wire                                    save_read_buffer_data_valid,
+  input wire  [C_M_AXI_DATA_WIDTH-1:0]          save_read_buffer_data      ,
+  // ctrl signals connected with ctrl module, use it
+  input wire                                    ap_start           ,
+  output wire                                   ap_done            ,
+  input wire  [C_M_AXI_ADDR_WIDTH-1:0]          ctrl_addr_offset   ,
+  input wire  [SAVE_INST_LENGTH  -1:0]          ctrl_instruction   ,
+  input wire                                   data_tready        ,
+  output logic                                  data_tvalid        ,
+  output logic [C_M_AXI_DATA_WIDTH-1:0]         data_tdata
+);
+
+timeunit 1ps;
+timeprecision 1ps;
+
+///////////////////////////////////////////////////////////////////////////////
+// Local Parameters
+///////////////////////////////////////////////////////////////////////////////
+localparam integer LP_DW_BYTES             = C_M_AXI_DATA_WIDTH/8;
+localparam integer LP_AXI_BURST_LEN        = 4096/LP_DW_BYTES < 256 ? 4096/LP_DW_BYTES : 256;
+localparam integer LP_LOG_BURST_LEN        = $clog2(LP_AXI_BURST_LEN);
+localparam integer LP_BRAM_DEPTH           = 512;
+localparam integer LP_RD_MAX_OUTSTANDING   = LP_BRAM_DEPTH / LP_AXI_BURST_LEN;
+localparam integer LP_WR_MAX_OUTSTANDING   = 32;
+
+///////////////////////////////////////////////////////////////////////////////
+// Wires and Variables
+///////////////////////////////////////////////////////////////////////////////
+
+// Control logic
+logic                          done = 1'b0;
+// writting ctrl port, use it
+logic [C_M_AXI_ADDR_WIDTH-1:0] dram_xfer_start_addr;
+logic [C_XFER_SIZE_WIDTH-1:0] dram_xfer_size_in_bytes;
+// writting data port stage, use it
+// AXI write master stage, use it 
+logic                          write_done;
+logic                          write_start;
+
+
+logic [C_M_AXI_DATA_WIDTH-1:0] FIFO [0:4-1];
+logic [1:0] FIFO_head;
+logic [1:0] FIFO_tail;
+logic [2:0] FIFO_count;
+logic [11-1:0] buffer_cur_addr;
+logic [11-1:0] buffer_end_addr;
+logic [2:0] clk_cnt;
+
+///////////////////////////////////////////////////////////////////////////////
+// Begin RTL
+///////////////////////////////////////////////////////////////////////////////
+
+
+always @(posedge aclk or posedge areset) begin
+  if (areset) begin
+    write_start <= 1'b0;
+    dram_xfer_start_addr <= 0;
+    dram_xfer_size_in_bytes <= 0;
+    buffer_cur_addr <= 0;
+    buffer_end_addr <= 0;
+    FIFO_head <= 0;
+    FIFO_tail <= 0;
+    FIFO_count <= 0;
+    FIFO[0] <= 0;
+    FIFO[1] <= 0;
+    FIFO[2] <= 0;
+    FIFO[3] <= 0;
+    save_read_buffer_addr_valid <= 0;
+    data_tvalid <= 0;
+    clk_cnt <= 0;
+  end else begin
+    if (ap_start) begin
+      write_start <= 1'b1;
+      write_done <= 1'b0;
+      dram_xfer_start_addr <= ctrl_instruction[SAVE_INST_LENGTH-17:SAVE_INST_LENGTH-32];  
+      dram_xfer_size_in_bytes <= ctrl_instruction[SAVE_INST_LENGTH-1:SAVE_INST_LENGTH-16]; 
+      buffer_cur_addr <= ctrl_instruction[SAVE_INST_LENGTH-49:SAVE_INST_LENGTH-64]; 
+      buffer_end_addr <= ctrl_instruction[SAVE_INST_LENGTH-49:SAVE_INST_LENGTH-64] + ctrl_instruction[SAVE_INST_LENGTH-33:SAVE_INST_LENGTH-48];
+      save_read_buffer_addr_valid <= 0;
+      data_tvalid <= 0;
+    end
+
+    if (ap_done) begin
+      write_start <= 1'b0;
+      dram_xfer_start_addr <= 0;
+      dram_xfer_size_in_bytes <= 0;
+      buffer_cur_addr <= 0;
+      buffer_end_addr <= 0;
+      save_read_buffer_addr_valid <= 0;
+      data_tvalid <= 0;
+      clk_cnt <= 0;
+    end
+
+    if (write_start) begin
+      if (save_read_buffer_data_valid) begin
+        if (data_tready) begin
+          if (FIFO_count == 0) begin
+            data_tvalid <= 1'b1;
+            data_tdata <= save_read_buffer_data;
+          end else begin
+            data_tvalid <= 1'b1;
+            data_tdata <= FIFO[FIFO_head];
+            FIFO_head <= FIFO_head + 1;
+            FIFO[FIFO_tail] <= save_read_buffer_data;
+            FIFO_tail <= FIFO_tail + 1;
+          end
+        end else begin
+          data_tvalid <= 1'b0;
+          FIFO[FIFO_tail] <= save_read_buffer_data;
+          FIFO_tail <= FIFO_tail + 1;
+          FIFO_count <= FIFO_count + 1;
+        end
+      end else begin
+        if (data_tready) begin    
+          if (FIFO_count > 0) begin
+            data_tvalid <= 1'b1;
+            data_tdata <= FIFO[FIFO_head];
+            FIFO_head <= FIFO_head + 1;
+            FIFO_count <= FIFO_count - 1;
+          end
+          else begin
+            data_tvalid <= 1'b0;
+          end
+        end else begin
+          data_tvalid <= 1'b0;
+        end
+      end
+
+      if (buffer_cur_addr == buffer_end_addr)
+        clk_cnt <= clk_cnt + 1;
+      if (data_tready && FIFO_count == 0 && buffer_cur_addr < buffer_end_addr) begin
+        save_read_buffer_addr_valid <= 1'b1;
+        save_read_buffer_addr <= buffer_cur_addr;
+        buffer_cur_addr <= buffer_cur_addr + 1;
+      end else if (clk_cnt>=4 && FIFO_count == 0)
+        write_done <= 1'b1;
+      else begin
+        save_read_buffer_addr_valid <= 1'b0;
+      end
+    end
+  end
+end
+
+
+
+
+assign ap_done = write_done;
+
+endmodule : gnn_0_example_save
+`default_nettype wire
+
---
+++ kernel_verilog/sim/save/save_tb.sv
@@ -0,0 +1,115 @@
+`default_nettype none
+
+parameter integer SAVE_INST_LENGTH         = 96;
+parameter integer C_M_AXI_ADDR_WIDTH       = 64;
+parameter integer C_M_AXI_DATA_WIDTH       = 512;
+parameter integer C_XFER_SIZE_WIDTH        = 32;
+parameter integer C_ADDER_BIT_WIDTH        = 32;
+
+module tb_gnn_0_example_save;
+reg                                      aclk               ;
+reg                                      arst_n             ; 
+logic                                    kernel_clk         ;
+logic                                    kernel_rst         ;
+logic                                    m_axi_awvalid      ;
+logic                                    m_axi_awready      ;
+logic [C_M_AXI_ADDR_WIDTH  -1:0]         m_axi_awaddr       ;
+logic [8-1:0]                            m_axi_awlen        ;
+logic                                    m_axi_wvalid       ;
+logic                                    m_axi_wready       ;
+logic [C_M_AXI_DATA_WIDTH  -1:0]         m_axi_wdata        ;
+logic [C_M_AXI_DATA_WIDTH/8-1:0]         m_axi_wstrb        ;
+logic                                    m_axi_wlast        ;
+logic                                    m_axi_bvalid       ;
+logic                                    m_axi_bready       ;
+logic                                    save_read_buffer_addr_valid;
+logic [11-1:0]                           save_read_buffer_addr      ;
+logic                                    save_read_buffer_data_valid;
+logic  [C_M_AXI_DATA_WIDTH-1:0]          save_read_buffer_data      ;
+logic                                    ap_start           ;
+logic                                    ap_done            ;
+logic  [C_M_AXI_ADDR_WIDTH-1:0]          ctrl_addr_offset   ;
+logic  [SAVE_INST_LENGTH  -1:0]          ctrl_instruction   ;
+logic                                    data_tready         ;
+logic                                    data_tvalid          ;
+logic [C_M_AXI_DATA_WIDTH-1:0]           data_tdata           ;
+
+gnn_0_example_save i_save
+(
+    .aclk                       (aclk              ),
+    .areset                     (arst_n            ),
+    .kernel_clk                 (kernel_clk        ),
+    .kernel_rst                 (kernel_rst        ),
+    .m_axi_awvalid              (m_axi_awvalid     ),
+    .m_axi_awready              (m_axi_awready     ),
+    .m_axi_awaddr               (m_axi_awaddr      ),
+    .m_axi_awlen                (m_axi_awlen       ),
+    .m_axi_wvalid               (m_axi_wvalid      ),
+    .m_axi_wready               (m_axi_wready      ),
+    .m_axi_wdata                (m_axi_wdata       ),
+    .m_axi_wstrb                (m_axi_wstrb       ),
+    .m_axi_wlast                (m_axi_wlast       ),
+    .m_axi_bvalid               (m_axi_bvalid      ),
+    .m_axi_bready               (m_axi_bready      ),
+    .save_read_buffer_addr_valid(save_read_buffer_addr_valid),
+    .save_read_buffer_addr      (save_read_buffer_addr      ),
+    .save_read_buffer_data_valid(save_read_buffer_data_valid),
+    .save_read_buffer_data      (save_read_buffer_data      ),
+    .ap_start                   (ap_start          ),
+    .ap_done                    (ap_done           ),
+    .ctrl_addr_offset           (ctrl_addr_offset  ),
+    .ctrl_instruction           (ctrl_instruction  ),
+    .data_tready                (data_tready       ),
+    .data_tvalid                (data_tvalid       ),
+    .data_tdata                 (data_tdata        )
+);
+
+localparam CLK_PERIOD = 10;
+localparam DRAM_START = 16'b1100110000110011;
+localparam DRAM_SIZE  = 16'b000001000000000;
+localparam BUFFER_START = 16'b1100110000110011;
+localparam BUFFER_SIZE = 16'b0000000000010000;
+localparam BLANK = 32'b0;
+localparam INST = {DRAM_SIZE, DRAM_START, BUFFER_SIZE, BUFFER_START, BLANK};
+integer i;
+always #(CLK_PERIOD/2) aclk=~aclk;
+
+logic [C_M_AXI_ADDR_WIDTH-1:0] addr_buffer[0:4-1];
+
+task accept_return_data();
+    begin
+        if (addr_buffer[4-1] != 0)
+        begin
+            save_read_buffer_data_valid <= 1'b1;
+            save_read_buffer_data <= addr_buffer[4-1];
+        end
+        else
+            save_read_buffer_data_valid <= 1'b0;
+        addr_buffer[4-1] <= addr_buffer[3-1];
+        addr_buffer[3-1] <= addr_buffer[2-1];
+        addr_buffer[2-1] <= addr_buffer[1-1];
+        if (save_read_buffer_addr_valid)
+            addr_buffer[0] <= save_read_buffer_addr;
+        else
+            addr_buffer[0] <= 0;
+        
+    end
+endtask
+
+initial begin
+    #1 arst_n<=1'bx;aclk<=1'bx;data_tready<=1'b1;
+    #(CLK_PERIOD*3) arst_n<=1;ctrl_instruction<=INST;ap_start<=1;
+    #(CLK_PERIOD*3) arst_n<=0;aclk<=0;
+    for (i = 0; i < 100; i = i + 1) begin
+        #CLK_PERIOD;
+        if (i == 1)
+            ap_start <= 0;
+        if (i)
+        data_tready <= $random%2;
+        accept_return_data();
+    end
+    $finish(2);
+end
+
+endmodule
+`default_nettype wire
