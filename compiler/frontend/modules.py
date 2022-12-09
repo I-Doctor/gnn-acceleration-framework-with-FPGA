6b6fc85cf4886c41da409631486bfb83dbf37463
zhang677 <ZhangghLCN@163.com>
2022-12-09 03:20:19 +0000

    Add sage agg

--- compiler/frontend/modules.py
+++ compiler/frontend/modules.py
@@ -24,15 +24,14 @@ class SAGE(nn.Module):
         super().__init__()
         self.layers = nn.ModuleList()
         # two-layer GraphSAGE-mean
-        self.layers.append(dglnn.SAGEConv(in_size, hid_size, aggregation_type, bias=False))
-        self.layers.append(dglnn.SAGEConv(hid_size, out_size, aggregation_type, bias=False))
+        self.layers.append(dglnn.SAGEConv(in_size, hid_size, aggregator_type=aggregation_type, bias=False, activation=F.relu))
+        self.layers.append(dglnn.SAGEConv(hid_size, out_size, aggregator_type=aggregation_type, bias=False))
         self.dropout = nn.Dropout(0.5)
 
-    def forward(self, graph, x):
-        h = self.dropout(x)
-        for l, layer in enumerate(self.layers):
-            h = layer(graph, h)
-            if l != len(self.layers) - 1:
-                h = F.relu(h)
+    def forward(self, g, features):
+        h = features
+        for i, layer in enumerate(self.layers):
+            if i != 0:
                 h = self.dropout(h)
+            h = layer(g, h)
         return h
