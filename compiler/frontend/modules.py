c244243903593f783056453063f2de9224fe1781
zhang677 <ZhangghLCN@163.com>
2022-12-15 13:04:30 +0000

    Add bias for gcn

--- compiler/frontend/modules.py
+++ compiler/frontend/modules.py
@@ -2,13 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import dgl.nn as dglnn
 
+# Model definination is the same as https://github.com/dmlc/dgl/blob/0.9.1/examples/pytorch/gcn/train.py
 class GCN(nn.Module):
     def __init__(self, in_size, hid_size, out_size):
         super().__init__()
         self.layers = nn.ModuleList()
         # two-layer GCN
-        self.layers.append(dglnn.GraphConv(in_size, hid_size, bias=False, activation=F.relu))
-        self.layers.append(dglnn.GraphConv(hid_size, out_size, bias=False))
+        self.layers.append(dglnn.GraphConv(in_size, hid_size, activation=F.relu))
+        self.layers.append(dglnn.GraphConv(hid_size, out_size))
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, g, features):
@@ -19,13 +20,14 @@ class GCN(nn.Module):
             h = layer(g, h)
         return h
 
+# Model defination is the same as https://github.com/dmlc/dgl/blob/0.9.1/examples/pytorch/graphsage/train_full.py
 class SAGE(nn.Module):
     def __init__(self, in_size, hid_size, out_size, aggregation_type='mean'):
         super().__init__()
         self.layers = nn.ModuleList()
         # two-layer GraphSAGE-mean
-        self.layers.append(dglnn.SAGEConv(in_size, hid_size, aggregator_type=aggregation_type, bias=False, activation=F.relu))
-        self.layers.append(dglnn.SAGEConv(hid_size, out_size, aggregator_type=aggregation_type, bias=False))
+        self.layers.append(dglnn.SAGEConv(in_size, hid_size, aggregator_type=aggregation_type, activation=F.relu))
+        self.layers.append(dglnn.SAGEConv(hid_size, out_size, aggregator_type=aggregation_type))
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, g, features):
