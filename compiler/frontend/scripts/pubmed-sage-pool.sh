5bc72e85b5f15bd371bd32b52c64c8b30f5aa7b7
zhang677 <ZhangghLCN@163.com>
2022-12-20 11:45:56 +0000

    Fix doc

--- compiler/frontend/scripts/pubmed-sage-pool.sh
+++ compiler/frontend/scripts/pubmed-sage-pool.sh
@@ -3,7 +3,7 @@ hidden=$2
 layer=2
 model=sage
 agg=pool
-dataset=reddit
+dataset=pubmed
 root1="$model-$agg-$layer-$hidden-$dataset"
 python ../train.py --dataset $dataset --epochs $epochs --train --model $model --agg $agg
 python ../trace.py --root $root1 --dataset $dataset --model $model
--- compiler/frontend/scripts/test.sh
+++ compiler/frontend/scripts/test.sh
@@ -1,19 +1,8 @@
 epochs=$1
 hidden=$2
 layer=2
-
 model=sage
-dataset=enzymes
-agg=mean
-root1="$model-$agg-$layer-$hidden-$dataset"
-python ../train.py --dataset $dataset --epochs $epochs --train --model $model --agg $agg
-python ../trace.py --root $root1 --dataset $dataset --model $model
-python ../train.py --dataset $dataset --model $model --agg $agg
-python ../check.py --root $root1 --dataset $dataset
-
-
-model=sage
-for dataset in pubmed cora citeseer; do
+for dataset in pubmed cora citeseer reddit; do
   for agg in mean gcn pool; do
     root1="$model-$agg-$layer-$hidden-$dataset"
     python ../train.py --dataset $dataset --epochs $epochs --train --model $model --agg $agg
@@ -24,7 +13,7 @@ for dataset in pubmed cora citeseer; do
 done
 
 model=gcn
-for dataset in pubmed cora citeseer; do
+for dataset in pubmed cora citeseer reddit; do
   root1="$model-$layer-$hidden-$dataset"
   python ../train.py --dataset $dataset --epochs $epochs --train --model $model
   python ../trace.py --root $root1 --dataset $dataset --model $model
