d8340a8c4afa9bb1d0c1fe48619391a275db7382
Jin Si <79039778+Jagnate@users.noreply.github.com>
2023-02-26 13:35:46 +0800

    Add files via upload

---
+++ kernel_verilog/src/mm/new/vector_add.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 10ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/12/09 18:38:39
+// Design Name: 
+// Module Name: vector_add
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vector_add(
+    input clk,
+    input [511:0]vector_1,
+    input [511:0]vector_2,
+    input vector_input_valid,
+    
+    output vector_output_valid,
+    output [511:0]vector
+    );
+    
+    genvar i;
+    
+    generate 
+        for(i=0;i<16;i=i+1)
+        begin
+            floating_point_add u_floating_point_add(
+              .aclk(clk),
+              .s_axis_a_tvalid(vector_input_valid),
+              .s_axis_a_tdata(vector_1[((i+1)*32)-1:i*32]),
+              .s_axis_b_tvalid(vector_input_valid),
+              .s_axis_b_tdata(vector_2[((i+1)*32)-1:i*32]),
+              .m_axis_result_tvalid(vector_output_valid),
+              .m_axis_result_tdata(vector[((i+1)*32)-1:i*32]) 
+            );
+        end
+    endgenerate
+    
+endmodule
